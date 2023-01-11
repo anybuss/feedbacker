832e4e4221d99f87fdf2ce2d958c054cd7faf88c
Any Buss <anykbuss@gmail.com>
2023-01-10 22:59:39 -0300

    create footer

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,9 +1,48 @@
-<script setup></script>
-
 <template>
   <main>
     <router-view></router-view>
   </main>
+
+  <footer>
+    <p>
+      Feedbacker &copy; <span>{{ getCurrentYear() }}</span>
+    </p>
+  </footer>
+
+  <copyright-dev></copyright-dev>
 </template>
 
-<style lang="scss"></style>
+<script setup>
+import CopyrightDev from "@/components/CopyrightDev.vue";
+
+function getCurrentYear() {
+  return new Date().getFullYear();
+}
+</script>
+
+<style lang="scss">
+body,
+#app {
+  font-family: $font-family;
+  font-weight: $font-regular;
+  color: $graydark-color;
+}
+
+main {
+  margin: 0 auto;
+  min-height: calc((100vh - 80px) - 40px);
+}
+
+footer {
+  background-color: $gray-color;
+  padding: 2rem 0;
+  height: 80px;
+  p {
+    color: $graydark-color;
+    font-size: 18px;
+    font-weight: $font-bold;
+    text-align: center;
+    text-transform: lowercase;
+  }
+}
+</style>
