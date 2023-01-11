43a6d53dc9cd669971c43257535b326f2ab0812e
Any Buss <anykbuss@gmail.com>
2023-01-10 22:51:09 -0300

    create copyright component

---
+++ dashboard/src/components/CopyrightDev.vue
@@ -0,0 +1,33 @@
+<template>
+  <aside>
+    <p>
+      Projeto base por
+      <a href="https://treinamento.vuejsbrasil.org/">
+        Treinamento - VueJS Brasil</a
+      >,
+    </p>
+    <p>
+      desenvolvido por
+      <a href="https://www.linkedin.com/in/anybuss/">Any Buss</a> &#128420;
+    </p>
+  </aside>
+</template>
+
+<style lang="scss" scoped>
+aside {
+  padding: 0.5rem 0;
+  height: 40px;
+  p {
+    color: $graymedium-color;
+    font-size: 14px;
+    text-align: center;
+    a {
+      color: $graydark-color;
+      text-decoration: none;
+      &:hover {
+        color: $main-color;
+      }
+    }
+  }
+}
+</style>
