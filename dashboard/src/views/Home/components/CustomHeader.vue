d5dad515eb39ebe061c3741859687c3f0761500a
Any Buss <anykbuss@gmail.com>
2023-01-12 19:02:55 -0300

    create customHeader template

---
+++ dashboard/src/views/Home/components/CustomHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <div class="wrapper">
+      <div class="top-container">
+        <img src="@/assets/images/logo_white.svg" alt="Feedbacker logo" />
+        <div class="wrapper-buttons">
+          <button class="btn-create">Crie uma conta</button>
+          <button class="btn-login">Entrar</button>
+        </div>
+      </div>
+
+      <div class="bottom-container">
+        <h1>
+          Tenha um feedback. <br />
+          E faça seus clientes mais <br class="line-break" />
+          felizes!
+        </h1>
+        <p>
+          Receba ideias, reclamações e feedbacks com um
+          <br class="line-break" />
+          simples widget na página.
+        </p>
+        <div>
+          <button>Crie uma conta grátis</button>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
