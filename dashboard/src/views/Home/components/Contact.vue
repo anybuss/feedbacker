b11adf9a730b9752e2ff472284ac2fc8770964b6
Any Buss <anykbuss@gmail.com>
2023-01-11 21:47:14 -0300

    add SCSS styles to contact component

--- dashboard/src/views/Home/components/Contact.vue
+++ dashboard/src/views/Home/components/Contact.vue
@@ -10,4 +10,43 @@
   </section>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+section {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80%;
+    max-width: 1200px;
+    margin: 4rem 0;
+    h1 {
+      font-weight: $font-black;
+      font-size: 2.25rem;
+      text-align: center;
+      color: $graydark-color;
+    }
+    p {
+      font-size: 1.25rem;
+      line-height: 1.75rem;
+    }
+    .wrapper-link {
+      margin-top: 2.5rem;
+      a {
+        color: #ffffff;
+        background-color: $main-color;
+        font-weight: $font-bold;
+        padding: 0.5rem 1.5rem;
+        margin-top: 2.5rem;
+        border-radius: 9999px;
+        text-decoration: none;
+        &:focus {
+          outline: none;
+        }
+      }
+    }
+  }
+}
+</style>
