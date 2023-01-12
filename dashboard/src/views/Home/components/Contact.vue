eb0563b879e7641f46584077f3e28307c3c8d9c3
Any Buss <anykbuss@gmail.com>
2023-01-11 21:45:37 -0300

    create Contact component template

---
+++ dashboard/src/views/Home/components/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <div class="wrapper">
+      <h1>Alguma dúvida?</h1>
+      <p>Quer saber melhor como funciona e quais são os preços?</p>
+      <div class="wrapper-link">
+        <a href="mailto:"> Nos mande um e-mail! </a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss"></style>
