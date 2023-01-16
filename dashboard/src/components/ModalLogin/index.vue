c3bc5f2f39efb0e2c5ea5bf6717fa3740372d834
Any Buss <anykbuss@gmail.com>
2023-01-16 19:11:18 -0300

    initial ModalLogin template

--- dashboard/src/components/ModalLogin/index.vue
+++ dashboard/src/components/ModalLogin/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <h1>ModalLogin</h1>
+  <section>
+    <div class="top-content">
+      <h2>Entre na sua conta</h2>
+      <button @click="">&times;</button>
+    </div>
+
+    <fieldset>
+      <form @submit.prevent="">
+        <div class="form-item">
+          <label for="email">E-mail</label>
+          <input id="email" type="email" placeholder="exemplo@email.com" />
+        </div>
+
+        <div class="form-item">
+          <label for="password">Senha</label>
+          <input id="password" type="password" />
+        </div>
+
+        <button class="btn-submit" type="submit">Entrar</button>
+      </form>
+    </fieldset>
+  </section>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
