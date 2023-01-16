ac0f7de09f9e24427c39e0c1c92f0fbda196c462
Any Buss <anykbuss@gmail.com>
2023-01-16 19:12:33 -0300

    ModalLogin SCSS

--- dashboard/src/components/ModalLogin/index.vue
+++ dashboard/src/components/ModalLogin/index.vue
@@ -22,3 +22,61 @@
     </fieldset>
   </section>
 </template>
+
+<style lang="scss" scoped>
+section {
+  .top-content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    h2 {
+      font-weight: $font-black;
+      font-size: 1.75rem;
+      line-height: 1.75rem;
+    }
+    button {
+      @include button-custom;
+      font-size: 2rem;
+      color: $graymedium-color;
+    }
+  }
+
+  fieldset {
+    form {
+      .form-item {
+        margin-top: 1.5rem;
+        label {
+          display: block;
+          font-weight: $font-black;
+          font-size: 1.15rem;
+          margin-bottom: 0.5rem;
+        }
+        input[type="email"],
+        input[type="password"] {
+          display: block;
+          width: 100%;
+          padding: 0.75rem 1rem;
+          background-color: $gray-color;
+          border-width: 2px;
+          border-color: transparent;
+          border-radius: 0.25rem;
+          &:focus {
+            outline: none;
+          }
+        }
+        span {
+          color: $danger-color;
+          display: block;
+          font-weight: $font-medium;
+        }
+      }
+      .btn-submit {
+        @include button-action;
+        margin-top: 2rem;
+        font-size: 1.15rem;
+        transition: all 150ms;
+      }
+    }
+  }
+}
+</style>
