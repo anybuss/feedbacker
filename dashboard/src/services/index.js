4414ba3fea5e8ad9ab235a403b6c8166a4f660b2
Any Buss <anykbuss@gmail.com>
2023-01-24 18:24:05 -0300

    create httpClient with axios

---
+++ dashboard/src/services/index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import AuthService from "./auth";
+
+const API_ENVS = {
+  local: "http://localhost:3000",
+  production: "",
+};
+
+const httpClient = axios.create({
+  baseURL: API_ENVS.local,
+});
+
+export default {
+  auth: AuthService(httpClient),
+};
