318d8523943bc782da6a8f0b27f53b424fbc617e
Any Buss <anykbuss@gmail.com>
2023-01-24 19:38:37 -0300

    create icon component

---
+++ dashboard/src/components/Icon/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="handleIconReference(name)" v-bind="$props"></component>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from "vue";
+
+const Loading = defineAsyncComponent(() => import("./Loading.vue"));
+const Close = defineAsyncComponent(() => import("./Close.vue"));
+
+const props = defineProps({
+  name: { type: String, required: true },
+});
+
+function handleIconReference(name) {
+  if (name === "loading") return Loading;
+  if (name === "close") return Close;
+}
+</script>
