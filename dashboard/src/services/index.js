3329c1f8fe6eadd57bbaf0c84dfea8cba3f0322b
Any Buss <anykbuss@gmail.com>
2023-01-25 19:22:44 -0300

    create service  to get user logged

--- dashboard/src/services/index.js
+++ dashboard/src/services/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+import router from "@/router";
 import AuthService from "./auth";
+import UsersService from "./users";
 
 const API_ENVS = {
   local: "http://localhost:3000",
@@ -8,6 +10,14 @@ const API_ENVS = {
 
 const httpClient = axios.create({
   baseURL: API_ENVS.local,
+});
+
+httpClient.interceptors.request.use((config) => {
+  const token = window.localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 httpClient.interceptors.response.use(
@@ -19,10 +29,16 @@ httpClient.interceptors.response.use(
     if (canThrowAnError) {
       throw new Error(error.message);
     }
+
+    if (error.response.status === 401) {
+      router.push({ name: "home" });
+    }
+
     return error;
   }
 );
 
 export default {
   auth: AuthService(httpClient),
+  users: UsersService(httpClient),
 };
