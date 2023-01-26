4bf4453c10ace1685be2994853dc26dd6bcc2037
Any Buss <anykbuss@gmail.com>
2023-01-26 20:27:11 -0300

    add section-name

--- dashboard/src/views/Credentials/index.vue
+++ dashboard/src/views/Credentials/index.vue
@@ -1,7 +1,30 @@
 <template>
   <header-logged></header-logged>
+
+  <section>
+    <div class="section-name">
+      <h1>Credenciais</h1>
+      <p>Guia de instalação e geração de suas credenciais.</p>
+    </div>
+  </section>
 </template>
 
 <script setup>
 import HeaderLogged from "@/components/HeaderLogged/index.vue";
 </script>
+
+<style lang="scss" scoped>
+section {
+  .section-name {
+    @include section-name;
+    h1 {
+      @include section-name-title;
+    }
+    p {
+      font-size: 1.25rem;
+      line-height: 1.75rem;
+      text-align: center;
+    }
+  }
+}
+</style>
