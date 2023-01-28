71fffac9dc9dd84b9900f255ccf42ff5a96208f6
Any Buss <anykbuss@gmail.com>
2023-01-27 23:26:47 -0300

    add header and section-banner

--- dashboard/src/views/Feedbacks/index.vue
+++ dashboard/src/views/Feedbacks/index.vue
@@ -1,7 +1,30 @@
 <template>
   <header-logged></header-logged>
+
+  <section>
+    <div class="section-name">
+      <h1>Feedbacks</h1>
+      <p>Detalhes de todos os feedbacks recebidos.</p>
+    </div>
+  </section>
 </template>
 
 <script setup>
 import HeaderLogged from "@/components/HeaderLogged/index.vue";
 </script>
+
+<style lang="scss" scoped>
+section {
+  .section-name {
+    @include section-name;
+    h1 {
+      @include section-name-title;
+    }
+    p {
+      font-size: 1.25rem;
+      line-height: 1.75rem;
+      text-align: center;
+    }
+  }
+}
+</style>
