ebe97ac28f5131afc256beee94b4a9c7f15a525f
Any Buss <anykbuss@gmail.com>
2023-01-31 22:03:02 -0300

    create feedback badge

---
+++ dashboard/src/views/Feedbacks/components/Badge.vue
@@ -0,0 +1,37 @@
+<template>
+  <span class="badge" :style="{ backgroundColor: `${classColor}` }">
+    {{ label }}
+  </span>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  type: { type: String, required: true },
+});
+
+const label = computed(() => {
+  if (props.type === "ISSUE") return "problema";
+  if (props.type === "IDEA") return "ideia";
+  return "outros";
+});
+
+const classColor = computed(() => {
+  if (props.type === "ISSUE") return "#F88676";
+  if (props.type === "IDEA") return "#E4B52E";
+  return "#EF4983";
+});
+</script>
+
+<style lang="scss" scoped>
+.badge {
+  color: $white-color;
+  font-weight: $font-black;
+  font-size: 0.75rem;
+  line-height: 1rem;
+  text-transform: lowercase;
+  border-radius: 999px;
+  padding: 0.5rem 1rem;
+}
+</style>
